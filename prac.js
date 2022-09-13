56bae4691d818ad54a37e6d6d239f950689fffe4
ABindusri <atyambindusri@gmail.com>
2022-09-13 16:46:07 +0530

    laddu first commit

---
+++ prac.js
@@ -0,0 +1,4 @@
+var student = {name:"sai",age:15,class:"9th",fathername:"Krishna",mothername:"gauri",
+                    father:{fatheroccupation:"teacher",address:"102,S.K.Residency"}
+};
+ Document.write(student);
